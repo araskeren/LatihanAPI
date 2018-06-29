c3cb54060f915423c7b7c896daf937bd490d6de5
araskeren <damis.bachtiar@gmail.com>
2018-06-29 14:48:08 +0700

    Menambah Relasi Database

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Product;
 class Review extends Model
 {
     protected $table='review';
+
+    public function product(){
+      return $this->belongsTo(Product::class);
+    }
 }
